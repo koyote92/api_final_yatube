c0c0c27caee5f5957b03c345b26143b168977cd5
Roman Inozemtsev <dev@koyote92.ru>
2023-02-26 14:02:20 +0500

    Added "select_related" to comments queryset

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -19,7 +19,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments
+        return post.comments.select_related('author', 'post')
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
